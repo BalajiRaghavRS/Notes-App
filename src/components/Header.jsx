6f1048a04b79eaeeb0df6a0417d1c85e9eeb124c
raghav <balaji.com>
2024-06-23 18:01:48 +0530

    Notes app

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import HighlightIcon from "@mui/icons-material/Highlight";
+import NotesIcon from '@mui/icons-material/Notes';
+function Header() {
+  return (
+    <header>
+      <center><h1><NotesIcon />
+      Notes </h1></center>
+    </header>
+  );
+}
+
+export default Header;
